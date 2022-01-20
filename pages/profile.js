98e1690b4f1aa3b2bc2dd3c288a3e715aeadfdeb
Matthias Lechien <matthias.lechien@gmail.com>
2022-01-20 19:18:11 +0100

    profile page

---
+++ pages/profile.js
@@ -0,0 +1,62 @@
+import styles from '../styles/Profile.module.scss';
+import NavBar from '../comps/navBar';
+
+import { FaRegEdit, FaMountain } from 'react-icons/fa';
+import { RiMenLine } from 'react-icons/ri';
+import { GiMountainClimbing, GiRoundStar, GiWarPick, GiMountaintop } from 'react-icons/gi';
+import { HiOutlineIdentification } from 'react-icons/hi';
+import { FiChevronDown } from 'react-icons/fi';
+import Image from 'next/image';
+
+export default function Profile() {
+    return (
+        <div className={styles.container}>
+
+            <NavBar />
+
+            <main>
+                <div className={styles.profile_info}>
+                    <h2>Profile <FaRegEdit color="#353535" /></h2>
+                    <div className={styles.user_infos}>
+                        <div className={styles.avatar_wraper}>
+                            <Image className={styles.avatar} src='/images/person4.jpg' layout='fill' objectFit='cover' />
+                            <span className={styles.country}><Image src="/icons/belgium.png" width={20} height={20} alt='country' /></span>
+                        </div>
+                        <div className={styles.infos}>
+                            <h6>Prénom Nom</h6>
+                            <p>Age <RiMenLine size="17px" color="#353535" style={{'margin-left': '15px'}} /></p>
+                            <p>Langues/Langues</p>
+                        </div>
+                    </div>
+                    <div className={styles.date}>Membre depuis le 17/12/2021</div>
+                    <div className={styles.level}>
+                        <p>Niveau Général : Confirmé</p>
+                        <p><GiMountainClimbing color="#353535" size="3vh" style={{'margin-right': '20px'}} />Rocher : 7b</p>
+                        <p><GiWarPick color="#353535" size="3vh" style={{'margin-right': '20px'}} />Glace : 5</p>
+                        <p><GiMountaintop color="#353535" size="3vh" style={{'margin-right': '20px'}} />Montagne : ED</p>
+                    </div>
+                    <div><a href="#"><GiRoundStar />12 Avis</a></div>
+                    <div><FaMountain /><a href="#">12 Expéditions finies</a></div>
+                    <div><FaMountain /><a href="#">3 Expéditions postées</a></div>
+                    <span><HiOutlineIdentification />Identité vérifiée</span>
+                    <p>
+                        Salut,
+                        Moi c’est Matthias, j’arpente les montagnes depuis un bon bout de temps maintenant.
+                        J’ai un bon niveau en rocher et surtout un bon sang-froid en montagne. Je suis tout
+                        à fait à l’aise en premier comme en second. Je suis toujours à la recherche de nouveaux
+                        partenaires pour partir en montagne, si t’as un projet qui match, ça te dise qu’on parte
+                        ensemble ? Fille ou garçon, débutant ou exp...
+                    </p>
+                    <div className={styles.see_all}>
+                        <p>Voir Tout</p>
+                        <FiChevronDown />
+                    </div>
+                </div>
+
+                <div className={styles.profile_content}>
+
+                </div>
+            </main>
+        </div>
+    )
+}
